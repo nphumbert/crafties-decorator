b952d840ca0daff9e7b9940b983d0cd80887a34d
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2016-04-17 18:00:36 +0200

    feature(*): Use decorators to prevent null values and duplicates

--- src/main/java/com/crafties/decorator/Application.java
+++ src/main/java/com/crafties/decorator/Application.java
@@ -3,7 +3,7 @@ package com.crafties.decorator;
 public class Application {
 
     public static void main(String[] args) {
-        Box<String> box = new SimpleBox<>(10);
+        Box<String> box = new UniqueItemBox<>(new NonNullItemBox<>(new SimpleBox<>(10)));
         box.add("Banana");
         box.add(null);
         box.add("Papaya");
